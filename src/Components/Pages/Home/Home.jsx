8f657ac3236865002b89de9e08fbf41122319dfa
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-05-28 10:00:19 +0600

    provider wrap in home page

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
 
 const Home = () => {
     return (
-        <div>
+        <Provider store={store}>
             this is home
-        </div>
+        </Provider>
     );
 };
 
