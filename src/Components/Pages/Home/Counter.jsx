19b522657cfbd9da07dadf6122a4ceb04a0280c3
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-05-29 00:04:50 +0600

    call functions

--- src/Components/Pages/Home/Counter.jsx
+++ src/Components/Pages/Home/Counter.jsx
@@ -1,13 +1,21 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./Redux/features/Counter/counterSlice";
 
 const Counter = () => {
+
+    const { count } = useSelector(state => state.counter)
+    const dispatch = useDispatch();
+
     return (
         <div className="h-screen flex justify-center items-center">
             <div>
                 <div>
-                    <h1 className="text-5xl text-center mb-10">0</h1>
+                    <h1 className="text-5xl text-center mb-10">{count}</h1>
                 </div>
-                <button className="btn btn-md btn-primary mr-5">+</button>
-                <button className="btn btn-md btn-accent">-</button>
+
+                <button onClick={() => dispatch(increment())} className="btn btn-md btn-primary mr-5">+</button>
+
+                <button onClick={() => dispatch(decrement())} className="btn btn-md btn-accent">-</button>
             </div>
         </div>
     );
