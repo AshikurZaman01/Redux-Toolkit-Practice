640caf3457d317a3aee7aa282bdc275f8535cc7e
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-05-29 15:27:31 +0600

    use immet

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,8 +1,29 @@
 import { Provider } from 'react-redux';
 import { store } from './Redux/store';
 import Counter from './Counter';
+import { produce } from 'immer';
 
 const Home = () => {
+
+    let demon = [
+        'Arif',
+        'Sakib',
+        'Rahim',
+        'Rony',
+        'Tamim',
+        'Mashrafe',
+        'Shakib',
+        'Mahmudullah',
+    ]
+
+
+    const nextState = produce(demon, (draft) => {
+        draft.push('Rubel');
+        draft.push('Rakib');
+    })
+
+    console.log(nextState)
+
     return (
         <Provider store={store}>
             <Counter></Counter>
